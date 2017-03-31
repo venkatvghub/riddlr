4d6dc507b23f4769404d22099c9b2c379fd7bde0
Srinidhik Kaushik <shrinidhi.kaushik@gmail.com>
2017-04-01 02:40:50 +0530

    Fix typo in start-up.

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 # Runs on Guincorn with 32 workers.
 # The default port is 80.
 
-nohup gunicorn --access-logfile /tmp/flask-access.log --error-logfile /tmp/flask-error.log --bind 0.0.0.0:80 --workers 32 hame:app &
+nohup gunicorn --access-logfile /tmp/flask-access.log --error-logfile /tmp/flask-error.log --bind 0.0.0.0:80 --workers 32 game:app &
