c0e35de66cd183304b74a996ce725b2b811cd551
Srinidhik Kaushik <shrinidhi.kaushik@gmail.com>
2017-04-01 02:26:05 +0530

    Adds admin scripts.

---
+++ scripts/dba.py
@@ -0,0 +1,152 @@
+#! /usr/bin/env python2.7
+
+'''
+dba.py: Database administration; CRUD operations on events and users.
+'''
+
+__author__ = "Srinidhi Kaushik"
+__copyright__ = "Copyright (C) 2017 Srinidhi Kaushik"
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Srinidhi Kaushik"
+__email__ = "clickyotomy@"
+__status__ = "Production"
+
+
+import json
+import time
+import sqlite3
+import argparse
+
+
+def init(dbpath):
+    '''
+    Create a new database with `users` and `events` tables.
+    '''
+    dbconn = sqlite3.connect(dbpath)
+    cursor = dbconn.cursor()
+
+    cursor.execute('ATTACH DATABASE ? AS "game"', (dbpath,))
+
+    # Create a users table to store user information.
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS game.users
+            (
+                id          TEXT NOT NULL UNIQUE,
+                password    TEXT NOT NULL,
+                event       TEXT NOT NULL,
+                level       INTEGER NOT NULL DEFAULT -1,
+                email       TEXT NOT NULL,
+                phone       TEXT NOT NULL,
+                ban         BOOL DEFAULT 0,
+                timestamp   DATE
+            )
+        '''
+    )
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS game.events
+            (
+                id        TEXT NOT NULL UNIQUE,
+                data      TEXT NOT NULL,
+                current   BOOL DEFAULT 0,
+                timestamp DATE
+            )
+        '''
+    )
+
+    cursor.execute(
+        '''
+        CREATE INDEX IF NOT EXISTS game.index_user ON users(id)
+        '''
+    )
+
+    cursor.execute(
+        '''
+        CREATE INDEX IF NOT EXISTS game.index_event ON users(id)
+        '''
+    )
+
+    print (
+        'Initialized an empty database `game` with `users`, `tables` at `{}`.'
+    ).format(dbpath)
+
+    dbconn.commit()
+    dbconn.close()
+
+
+def load(dbpath, event, current=False):
+    '''
+    Load an event into the database.
+    '''
+    dbconn = sqlite3.connect(dbpath)
+    cursor = dbconn.cursor()
+
+    try:
+        if current:
+            cursor.execute(
+                'UPDATE events set current = ?', (not current)
+            )
+
+        cursor.execute(
+            'INSERT into events VALUES (?, ?, ?, ?)',
+            (event['id'], json.dumps(event), current, int(time.time()))
+        )
+
+    except sqlite3.IntegrityError:
+        print 'The event `{}` already exists, updating it.'.format(event['id'])
+        cursor.execute(
+            '''
+            UPDATE events
+            SET id = ?, current = ?, timestamp = ?, data = ?
+            WHERE id = ?
+            ''',
+            (
+                event['id'], current, int(time.time()),
+                json.dumps(event), event['id']
+            )
+        )
+
+    dbconn.commit()
+    dbconn.close()
+    print 'Database updated.'
+
+
+def main():
+    '''
+    The main method, accept and parse arguments.
+    '''
+    description = 'Interface for game database administration.'
+    init_help = 'init: initialize an empty database; '
+    load_help = 'load: insert (or update) an event to (on) the database'
+    event_help = 'file to load event data from'
+    dbpath_help = 'path to load/save the database from/to'
+    current_help = 'mark event as active (and the rest inactive)'
+
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument('-a', '--action', choices=('init', 'load'),
+                        help=''.join([init_help, load_help]),
+                        required=True)
+    parser.add_argument('-db', '--database-path', help=dbpath_help,
+                        required=True, default='game.db')
+    parser.add_argument('-c', '--current', action='store_true', default=False,
+                        help=current_help)
+    parser.add_argument('-f', '--from-file', help=event_help)
+
+    args = vars(parser.parse_args())
+
+    if args['action'] == 'load' and args['from_file'] is None:
+        print '{}: error: argument -f/--from-file is required'.format(__file__)
+        exit(1)
+
+    if args['action'] == 'load':
+        event = json.loads(open(args['from_file'], 'r').read())
+        load(args['database_path'], event, args['current'])
+    else:
+        init(args['database_path'])
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/stats.py
@@ -0,0 +1,192 @@
+#! /usr/bin/env python2.7
+
+'''
+stats.py: Fetch stats from the database.
+'''
+
+import re
+import time
+import json
+import argparse
+from operator import itemgetter
+
+import requests
+import tabulate
+
+
+# API URL of the server.
+API_URL = 'http://{}/sudo'.format
+
+# Fetch X-Auth-Token from `config.json`.
+APP_CONFIG_PATH = './config.json'
+
+
+def load_config():
+    '''
+    Load configuration variables from `config.json`.
+    '''
+    try:
+        with open(APP_CONFIG_PATH, 'r') as config:
+            return json.loads(config.read())
+    except (IOError, KeyError, ValueError) as err:
+        print ': '.join([type(err).__name__, str(err)]) + '.'
+        exit(1)
+
+
+# Authentication token. Must be the same value as `sudo` in `config.json`.
+X_AUTH_TOKEN = {
+    'X-Auth-Token': load_config()['sudo']
+}
+
+
+def http_debug(response):
+    '''
+    Print the request/response debug log.
+    '''
+    print 'http-request\n{0}\n'.format('-' * len('http-request'))
+    print 'url ({0}): {1}'.format(response.request.method,
+                                  response.request.url)
+    print 'request-headers:'
+    print json.dumps(dict(response.request.headers), indent=4)
+    if response.request.method != 'GET':
+        if response.request.body:
+            print 'request-payload:'
+            print json.dumps(json.loads(response.request.body), indent=4)
+    print '\nhttp-response\n{0}\n'.format('-' * len('http-response'))
+    print 'status-code: {0} {1}'.format(response.status_code, response.reason)
+    print 'url: {0}'.format(response.url)
+    print 'time-elapsed: {0}s'.format(response.elapsed.total_seconds())
+    print 'response-headers:'
+    print json.dumps(dict(response.headers), indent=4)
+    print 'response-content:'
+    try:
+        print None if response.content is '' else json.dumps(response.json(),
+                                                             indent=4)
+    except (KeyError, ValueError):
+        print '{}'
+
+
+def fetch(host, table, debug=False):
+    '''
+    Does an HTTP GET to the API endpoint.
+    '''
+    url = API_URL(host.strip())
+
+    try:
+        response = requests.get(
+            url, params={'table': table},
+            headers=X_AUTH_TOKEN, allow_redirects=False
+        )
+        if debug:
+            http_debug(response)
+
+        if response.status_code in range(200, 400):
+            return response.json()
+
+    except (ValueError, KeyError) as _error:
+        error = ': '.join([type(_error).__name__, str(_error)])
+        if debug:
+            print 'error:\n{0}\n{1}\n'.format('-' * len(error), error)
+
+    except (requests.exceptions.RequestException) as _error:
+        error = ': '.join([type(_error).__name__, str(_error)])
+        if debug:
+            print 'error:\n{0}\n{1}\n'.format('-' * len(error), error)
+
+    return None
+
+
+def constraints(filters):
+    '''
+    Filter out data, if required.
+    '''
+    data = fetch(filters['server'], filters['table'], filters['debug'])
+
+    if data is not None:
+        if filters['table'] == 'events':
+            return data['events']
+        else:
+            response = []
+            response = [_ for _ in data['users']
+                        if re.search(filters['filter'], _['id'])]
+            if not filters['ban']:
+                response = [_ for _ in response if not _['ban']]
+
+            response = [_ for _ in response if
+                        filters['since'] <= _['timestamp'] <= filters['until']
+                        ]
+            response = sorted(response, key=itemgetter('timestamp'),
+                              reverse=filters['reverse'])
+            response = response[:filters['limit']]
+
+            return response
+
+
+def main():
+    '''
+    Parse arguments, fetch data.
+    '''
+    description = 'Fetch stats from the game database.'
+    socket = 'hostname[:port] of the API server'
+    users = 'fetch data from the `users` table'
+    events = 'fetch data from the `events` table'
+    reverse = 'list active users in reverse chronological order.'
+    limit = 'limit the output to N users'
+    ban = 'include banned users in the output'
+    since = 'list users active since N (timestamp, UTC; default: epoch)'
+    until = 'list users active until N (timestamp, UTC; default: now)'
+    debug = 'enable debug logs'
+    user = 'A valid regular expression for filtering users (default: `.*``)'
+
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('-s', '--server', help=socket, required=True)
+
+    subparsers = parser.add_subparsers(
+        help='sub-commands that can be chosen from',
+        description='operations on tables', dest='table'
+    )
+
+    users = subparsers.add_parser('users', description=users)
+    events = subparsers.add_parser('events', description=events)
+
+    users.add_argument('-l', '--limit', type=int, help=limit, default=None)
+    users.add_argument(
+        '-r', '--reverse', help=reverse, action='store_true', default=False
+    )
+    users.add_argument(
+        '-b', '--ban', help=ban, action='store_true', default=False
+    )
+    users.add_argument('-s', '--since', help=since, default=0, type=int)
+    users.add_argument(
+        '-u', '--until', help=until, default=int(time.time()), type=int
+    )
+    users.add_argument(
+        '-d', '--debug', help=debug, action='store_true', default=False
+    )
+    users.add_argument(
+        '-f', '--filter', help=user, type=str, default='.*'
+    )
+    events.add_argument(
+        '-d', '--debug', help=debug, action='store_true', default=False
+    )
+
+    args = vars(parser.parse_args())
+
+    data = constraints(args)
+
+    if args['table'] == 'events':
+        for event in data:
+            print json.dumps(json.loads(event['data']), indent=4)
+            print 'current: {0}, timestamp: {1}.'.format(
+                                                    event['current'],
+                                                    event['timestamp']
+            )
+    else:
+        headers = data[0].keys() if len(data) else []
+        print tabulate.tabulate(
+                [_.values() for _ in data], headers=headers, tablefmt='psql')
+    print 'count: {}.'.format(len(data))
+
+
+if __name__ == '__main__':
+    main()
